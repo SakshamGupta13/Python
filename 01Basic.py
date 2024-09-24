06ed740c20044443fccfe88a99a28e7ba54d57b4
Saksham <guptasaksham795@gmail.com>
2024-09-24 15:48:58 +0530

    updated

---
+++ 01Basic.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+salary = 2500
+name = "Saksham Gupta"
+email = "guptaasaksham795@gmail.com"
+print(salary)
+print(name +" " +email,salary)
+
+print(F"My name is {name} and email is {email} salary is {salary}")
