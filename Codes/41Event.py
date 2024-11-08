d1c50abbc8927ec2d8d8b32c3ec7baf690d1df98
Saksham <guptasaksham795@gmail.com>
2024-11-08 22:10:53 +0530

    updated

---
+++ Codes/41Event.py
@@ -0,0 +1,6 @@
+# press 1 to add new event
+# press 2 to get all event
+# press 3 to delete event
+# press 4 to update event
+
+# press 5 to add stuudent in event :- stuName, stuName, stuEmail, stuMobile, styDept, stuYear, eventName
